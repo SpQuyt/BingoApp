d230540ff010b50956870144d0b1fe686d377597
Trương Anh Quốc <spquyt@gmail.com>
2022-01-30 21:24:07 +0700

    feature/init-base

---
+++ src/navigation/scene/TabScenes.tsx
@@ -0,0 +1,48 @@
+import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Images from 'assets/images';
+// Screen
+import HomeScreen from 'feature/home/HomeScreen';
+import SettingView from 'feature/setting/SettingScreen';
+import StyledTabBar from 'navigation/components/StyledTabBar';
+import navigationConfigs from 'navigation/config/options';
+import { TAB_NAVIGATION_ROOT } from 'navigation/config/routes';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { isIos } from 'utilities/helper';
+
+const MainStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+const HomeStack = () => (
+    <MainStack.Navigator headerMode={'none'} screenOptions={navigationConfigs} keyboardHandlingEnabled={isIos}>
+        <MainStack.Screen name={TAB_NAVIGATION_ROOT.HOME_ROUTE.HOME} component={HomeScreen} />
+    </MainStack.Navigator>
+);
+
+const MainTabContainer = () => {
+    const { t } = useTranslation();
+    const ArrayTabs = [
+        {
+            name: TAB_NAVIGATION_ROOT.HOME_ROUTE.ROOT,
+            title: t('tab.home'),
+            component: HomeStack,
+            icon: Images.icons.tab.home,
+        },
+        {
+            name: TAB_NAVIGATION_ROOT.SETTING_ROUTE.ROOT,
+            title: t('tab.setting'),
+            component: SettingView,
+            icon: Images.icons.tab.setting,
+        },
+    ];
+    return (
+        <MainTab.Navigator tabBar={(props: BottomTabBarProps) => <StyledTabBar {...props} />}>
+            {ArrayTabs.map((item, index) => (
+                <MainTab.Screen key={`${index}`} options={{ ...item }} {...item} />
+            ))}
+        </MainTab.Navigator>
+    );
+};
+
+export default MainTabContainer;
